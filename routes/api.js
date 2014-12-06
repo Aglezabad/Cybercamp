3f68ebbae27467912346e5573a96842566566ca9
Angel Gonzalez <aglezabad@gmail.com>
2014-12-06 21:52:55 +0100

    A la mierda TODO 2

--- routes/api.js
+++ routes/api.js
@@ -4,30 +4,12 @@ var User = ("../models/User");
 
 
 router.post('/login', function(req, res){
-	// passport.authenticate('local', function(err, user, info){
-	// 	if(err){
-	// 		return next(err);
-	// 	}
-	// 	if(!user){
-	// 		return res.send(info);
-	// 	}
-	// 	return res.send(user);
-	// })(req, res, next);
 	console.log(req.body);
 	res.send({ok: true});
 });
 
 router.post('/register', function(req, res){
-	// var newUser = new User({
-	// 	username: req.body.username,
-	// 	email: req.body.email
-	// });
-	// User.register(newUser, req.body.password, function(err, user) {
-	// 	if (err) {
-	// 		return next(err);
-	// 	}
-	// 	return res.send({ok: true});
-	// });
+	console.log(req.headers);
 	console.log(req.body);
 	res.send({ok: true});
 });
